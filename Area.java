26e2b76b6b5fab899ad538ca724de7980ea6e72f
juliangerber2020 <47428016+juliangerber2020@users.noreply.github.com>
2019-03-14 12:27:47 -0400

    Add files via upload

--- Area.java
+++ Area.java
@@ -7,44 +7,83 @@
  * Input: width, length
  * Process: area = width * length
  * Output: area
- /* 
- * Input: radius
- * Process: area = pi * r^2
- * Output: area
- */
+*/
 
 import java.util.Scanner; 
 public class Area {
+	static Scanner scnr = new Scanner(System.in);
+	 public static void main(String[] args) {
+		 
+			
 
-	public static void main (String[] args) {
-	Scanner scnr = new Scanner(System.in);
-	
-	System.out.println("Enter width: ");
-	double width = scnr.nextDouble();
-	
-	System.out.println("Enter length: ");
-	double length = scnr.nextDouble();
-	
-	double area = length * width;
-	
-	System.out.println("The area is " + area);
+/* 
+* getRectArea: int
+* params: width, int, length, int
+* calculates area of rectangle
+*/		
+			
+			System.out.println("Enter width: ");
+			int width = scnr.nextInt();
+			
+			System.out.println("Enter length: ");
+			int length = scnr.nextInt();
+			
+			int rectarea = getRectArea(width, length); 
 
-	}
+			
+			System.out.println("The area is " + rectarea);
+
+/* 
+* Input: radius
+* Process: area = pi * r^2
+* Output: area
+*/
+
+			System.out.println("Enter radius: ");
+double radius = scnr.nextDouble();
+
+double circarea = getCircArea(radius);
+System.out.println("The area is " + circarea);
+/* 
+* Input: base, height
+* Process: area = (base * height) / 2
+* Output: area
+*/
+
+System.out.println("Enter base: "); 
+double base = scnr.nextDouble();
+
+System.out.println("Enter height: ");
+double height = scnr.nextDouble();
+
+double triarea = getTriArea(base, height);
+
+System.out.println("Area is " + triarea);
+
+
+
+		}
+		 
+	 
+		 public static int getRectArea(int width, int length) {
+				return width * length;
+				}
+	
+
+
+
+
+
+
+
+
+
+public static double getTriArea(double base, double height) {
+return (base * height) / 2;
 
 }
+public static double getCircArea(double radius) {
+return (Math.PI * radius * radius); 
 
-public static void main(String[] args) {
-	
-	
-	Scanner scnr = new Scanner(System.in);
-	
-	System.out.println("Enter radius: ");
-	double radius = scnr.nextDouble();
-	
-	double area = Math.PI * Math.pow(radius, 2.0);
-	
-	System.out.println("The area is: " + area);
-
-	}
-
+}
 }
