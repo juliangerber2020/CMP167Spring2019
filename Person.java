1553a24a1b515951f7aee06acbee8a1c9eac2e4d
juliangerber2020 <47428016+juliangerber2020@users.noreply.github.com>
2019-03-28 12:40:12 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+	public String name;
+	public int age;
+	public char gender;
+
+	public Person(String name, int age, char gender) {
+		 this.name = name;
+		 this.age= age;
+		 this.gender = gender; 
+	}
+}
