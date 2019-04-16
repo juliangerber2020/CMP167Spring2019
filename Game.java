e87a46975c4cffabeeaa3ebe8b205300f1a7911f
juliangerber2020 <47428016+juliangerber2020@users.noreply.github.com>
2019-04-16 12:30:57 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,41 @@
+package Week11;
+
+public class Game {
+	public static void main(String [] args) {
+		Human person1 = new Human();
+		
+		System.out.println("Hunger: " + person1.hunger);
+		System.out.println("Food: " + person1.food);
+		
+		
+		System.out.println("Eating Command");
+		person1.eat();
+		System.out.println("Eating Command");
+		person1.eat();
+		System.out.println("Eating Command");
+		person1.eat();
+		
+		System.out.println("Hunger: " + person1.hunger);
+		System.out.println("Food: " + person1.food);
+		
+		System.out.println("Buying food command");
+		person1.buyFood();
+		
+		System.out.println("Working command");
+		person1.work();
+		
+		System.out.println("Buying food command");
+		person1.buyFood();
+		
+		System.out.println("Working command");
+		person1.work();
+		System.out.println("Working command");
+		person1.work();
+		System.out.println("Eating Command");
+		person1.eat();
+		System.out.println("Working command");
+		person1.work();
+		
+	}
+
+}
